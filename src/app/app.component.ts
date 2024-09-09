01e956b6511601f7a1b1c490cd89eac48c7c24a7
Joey Molinski <molinski826@msn.com>
2024-09-09 14:48:27 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from './models/menu-item';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PizzaAngular';
+
+  allItems: MenuItem[]=[
+    {name: "Chicken Fingers", category:"Dinner", price: 11.99, vegetarian: false, soldOut: false},
+    {name: "Pizza", category:"Dinner", price: 11.99, vegetarian: true, soldOut: false},
+    {name: "Pepperoni Pizza", category:"Dinner", price: 12.99, vegetarian: false, soldOut: false},
+    {name: "Bread Sticks", category:"Side", price: 4.99, vegetarian: true, soldOut: true},
+    {name: "Wings", category:"Side", price: 8.99, vegetarian: false, soldOut: false},
+    {name: "Cinnamon Roll", category:"Dessert", price: 8.99, vegetarian: true, soldOut: false}
+
+  ]
+  total: number = 0;
+
+  addToOrder(price: number): void {
+      this.total += price;
+  }
+
 }
